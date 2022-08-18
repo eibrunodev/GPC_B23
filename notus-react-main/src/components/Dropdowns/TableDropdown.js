77566a639e87c03df99ed7ee4f12c8a5ff80969c
eibrunodev <eibrunodev@gmail.com>
2022-08-18 11:00:15 -0300

    Atualizando aplicação

---
+++ notus-react-main/src/components/Dropdowns/TableDropdown.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { createPopper } from "@popperjs/core";
+
+const NotificationDropdown = () => {
+  // dropdown props
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+  const btnDropdownRef = React.createRef();
+  const popoverDropdownRef = React.createRef();
+  const openDropdownPopover = () => {
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: "left-start",
+    });
+    setDropdownPopoverShow(true);
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+  return (
+    <>
+      <a
+        className="text-blueGray-500 py-1 px-3"
+        href="#pablo"
+        ref={btnDropdownRef}
+        onClick={(e) => {
+          e.preventDefault();
+          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+        }}
+      >
+       <i class="fas fa-filter"></i>
+        Filtrar por
+      </a>
+      <div
+        ref={popoverDropdownRef}
+        className={
+          (dropdownPopoverShow ? "block " : "hidden ") +
+          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+        }
+      >
+        <a
+          href="#pablo"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+           <i className="fas fa-circle text-teal-500 mr-2"></i>
+          Vendas concluídas
+        </a>
+        <a
+          href="#pablo"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          <i className="fas fa-circle text-red-500 mr-2"></i>
+          Vendas não concluídas
+        </a>
+        <a
+          href="#pablo"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          }
+          onClick={(e) => e.preventDefault()}
+        >
+          <i className="fas fa-circle text-orange-500 mr-2"></i>
+          Em análise
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default NotificationDropdown;
